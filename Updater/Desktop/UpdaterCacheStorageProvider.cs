d26e92ccb7f30ce66b1c407fcfbca9f36d8fb36a
Daniel Mirmilshteyn <daniel.mirmilshteyn@outlook.com>
2015-01-31 16:35:32 -0500

    Implemented desktop updater cache storage provider

---
+++ Updater/Desktop/UpdaterCacheStorageProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Updater.Desktop
+{
+    public class UpdaterCacheStorageProvider : IUpdaterCacheStorageProvider
+    {
+        private readonly string InstalledPackageFile = "InstalledPackages.xml";
+
+        public string CacheDirectory { get; private set; }
+
+        public UpdaterCacheStorageProvider(string cacheDirectory) {
+            this.CacheDirectory = cacheDirectory;
+        }
+
+        public XmlReader GetInstalledPackageMetadataReader() {
+            string path = Path.Combine(CacheDirectory, InstalledPackageFile);
+
+            if (File.Exists(path) == false) {
+                CreateEmptyInstalledPackageMetadataFile(path);
+            }
+
+            return XmlReader.Create(path);
+        }
+
+        private void CreateEmptyInstalledPackageMetadataFile(string path) {
+            XmlWriterSettings xmlWriterSettings = new XmlWriterSettings()
+            {
+                Indent = true,
+                IndentChars = "\t"
+            };
+
+            using (XmlWriter xmlWriter = XmlWriter.Create(path, xmlWriterSettings)) {
+                xmlWriter.WriteStartDocument();
+                xmlWriter.WriteStartElement("Packages");
+
+                xmlWriter.WriteEndElement();
+                xmlWriter.WriteEndDocument();
+            }
+        }
+    }
+}
--- Updater/Updater/IUpdaterCacheStorageProvider.cs
+++ Updater/Updater/IUpdaterCacheStorageProvider.cs
@@ -9,6 +9,11 @@ namespace Updater
 {
     public interface IUpdaterCacheStorageProvider
     {
-        XmlReader GetInstalledPackageCache();
+        /// <summary>
+        /// Gets the installed package metadata reader. Reads from the file-system, using the platform implementation. 
+        /// If the installed package metadata cache does not exist, it is created.
+        /// </summary>
+        /// <returns></returns>
+        XmlReader GetInstalledPackageMetadataReader();
     }
 }
