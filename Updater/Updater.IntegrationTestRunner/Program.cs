a07996769ae8ce80de1c58a182d29c0cf134e9a1
Daniel Mirmilshteyn <daniel.mirmilshteyn@outlook.com>
2015-01-30 22:46:07 -0500

    Initial commit for the integration test runner

---
+++ Updater/Updater.IntegrationTestRunner/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Updater.IntegrationTestRunner
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            string updateSite = ConfigurationManager.AppSettings["updateSite"];
+        }
+    }
+}
