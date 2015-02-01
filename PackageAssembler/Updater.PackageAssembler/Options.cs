355cdc29c4eb51515d5f12f0ff5335629ea53591
Daniel Mirmilshteyn <daniel.mirmilshteyn@outlook.com>
2015-02-01 05:19:14 -0500

    Added loading of package assembler command line arguments

---
+++ PackageAssembler/Updater.PackageAssembler/Options.cs
@@ -0,0 +1,39 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Updater.PackageAssembler
+{
+    class Options
+    {
+        [Option('d', "id", Required = true, HelpText = "Sets the id of the generated package")]
+        public int PackageId { get; set; }
+
+        [Option('n', "name", Required = true, HelpText = "Sets the name of the generated package")]
+        public string PackageName { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "Output package file")]
+        public string OutputFile { get; set; }
+
+        [Option('r', "repository", Required = true, HelpText = "The path to the existing repository file")]
+        public string RepositoryFile { get; set; }
+
+        [Option('i', "installation", Required = true, HelpText = "The installation file to be used in the package")]
+        public string InstallationFile { get; set; }
+
+        [Option('c', "contents", Required = true, HelpText = "The contents to include in the package")]
+        public string ContentsSource { get; set; }
+
+        [HelpOption]
+        public string GetUsage() {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine("Updater Package Assembler");
+
+            return stringBuilder.ToString();
+        }
+    }
+}
--- PackageAssembler/Updater.PackageAssembler/Program.cs
+++ PackageAssembler/Updater.PackageAssembler/Program.cs
@@ -9,7 +9,16 @@ namespace Updater.PackageAssembler
     public static class Program
     {
         public static void Main(string[] args) {
-
+            Options options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, options)) {
+                Console.WriteLine("Package Id: " + options.PackageId);
+                Console.WriteLine("Package Name: " + options.PackageName);
+                Console.WriteLine("Output File: " + options.OutputFile);
+                Console.WriteLine("Repository File: " + options.RepositoryFile);
+                Console.WriteLine("Installation File: " + options.InstallationFile);
+                Console.WriteLine("Contents Directory: " + options.ContentsSource);
+                Console.WriteLine();
+            }
         }
     }
 }
