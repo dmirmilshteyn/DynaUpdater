066da83764fdf0448672ce473f039e86844cfc3b
Daniel Mirmilshteyn <daniel.mirmilshteyn@outlook.com>
2015-02-01 01:05:50 -0500

    Added storage provider for access to application files

---
+++ Updater/Desktop/StorageProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Updater.Storage;
+
+namespace Updater.Desktop
+{
+    public class StorageProvider : IStorageProvider
+    {
+        string storageDirectory;
+
+        public StorageProvider(string storageDirectory) {
+            this.storageDirectory = storageDirectory;
+        }
+
+        public Stream CreateFile(string relativePath) {
+            return new FileStream(Path.Combine(this.storageDirectory, relativePath), FileMode.Create);
+        }
+
+        public Stream OpenFile(string relativePath) {
+            return new FileStream(Path.Combine(this.storageDirectory, relativePath), FileMode.Open);
+        }
+
+        public void DeleteFile(string relativePath) {
+            File.Delete(Path.Combine(this.storageDirectory, relativePath));
+        }
+
+        public void DeleteDirectory(string relativePath) {
+            Directory.Delete(Path.Combine(this.storageDirectory, relativePath));
+        }
+    }
+}
---
+++ Updater/Updater/Storage/IStorageProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Updater.Storage
+{
+    public interface IStorageProvider
+    {
+        Stream CreateFile(string relativePath);
+        Stream OpenFile(string relativePath);
+
+        void DeleteFile(string relativePath);
+        void DeleteDirectory(string relativePath);
+    }
+}
